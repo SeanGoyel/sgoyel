4b09d685d5f92cf56213eedb9ae102042070b8c0
Sean Goyel <seangoyel@hotmail.com>
2020-12-31 20:16:29 -0800

    Updates

---
+++ precache-manifest.2bb0d4058428e7b1dc7fe4feb3766098.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb14cc152deb24b0d77f50d14db602e",
+    "url": "/SGoyel/index.html"
+  },
+  {
+    "revision": "dc0e3833b21a837ced25",
+    "url": "/SGoyel/static/css/2.4ef2ce32.chunk.css"
+  },
+  {
+    "revision": "b1e1fee80ccf217cc290",
+    "url": "/SGoyel/static/css/main.22989134.chunk.css"
+  },
+  {
+    "revision": "dc0e3833b21a837ced25",
+    "url": "/SGoyel/static/js/2.d9d944ba.chunk.js"
+  },
+  {
+    "revision": "b1e1fee80ccf217cc290",
+    "url": "/SGoyel/static/js/main.ee927bde.chunk.js"
+  },
+  {
+    "revision": "c4c24bc68800c0263aff",
+    "url": "/SGoyel/static/js/runtime~main.ef00f464.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/SGoyel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/SGoyel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/SGoyel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/SGoyel/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2e758d77af1c47a61ac54749a2a3323.js"
+  "/SGoyel/precache-manifest.2bb0d4058428e7b1dc7fe4feb3766098.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SGoyel/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
